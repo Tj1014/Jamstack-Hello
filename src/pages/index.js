c838bc7210d3798843d2c18b181c4dc0f6e4820f
TJ <tj1014@gmail.com>
2018-09-08 22:18:38 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Generic page greeting</h1>
+    <p>Insert personalized message here.</p>
+    <p>Or here.</p>
+    <Link to="/page-2/">Page 2 is empty</Link>
+  </div>
+)
+
+export default IndexPage
